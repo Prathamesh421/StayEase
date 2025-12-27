81ad5f4e4f8907dfe61d405ac521ec5edc8b8f22
Prathamesh421 <prathugavandhare@gmail.com>
2025-12-28 01:32:05 +0530

    Project setup

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const initData = require("./data");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "694c1d44f0b335b649ff6f03",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
---
+++ models/listing.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Review = require("./review");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
+    },
+    url: {
+      type: String,
+      default:
+        "https://images.pexels.com/photos/35116150/pexels-photo-35116150.jpeg",
+      set: (v) =>
+        v === ""
+          ? "https://images.pexels.com/photos/35116150/pexels-photo-35116150.jpeg"
+          : v,
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
